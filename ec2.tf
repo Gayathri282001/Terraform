f3fc6f4c81d4b1f51b25c3f4793654193c748abb
Gayathri <nagagayathrigongati@gmail.com>
2025-07-01 15:00:36 +0530

    ec2 creation

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,12 @@
+# resource "aws_instance" "my-wish-i-decide"{
+#     ami = "ami-0b4f379183e5706b9"
+#     instance_type = "t2.micro"
+# }
+
+
 resource "aws_instance" "my-wish-i-decide"{
-    ami = "ami-0b4f379183e5706b9"
-    instance_type = "t2.micro"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    security_groups = [aws_security_group.allow_all.name]
+
 }

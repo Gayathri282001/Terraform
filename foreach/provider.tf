5a1596c37b785ce6f9d49b390186c650d89a08a5
Gayathri <nagagayathrigongati@gmail.com>
2025-07-03 10:42:25 +0530

    foreach

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,8 +5,17 @@ terraform {
       version = "6.0.0"
     }
   }
-}
 
+
+  backend "s3" { 
+    bucket = "roboshop-practice-demo"
+    key = "foreach-demo"
+    region = "us-east-1"
+
+     dynamodb_table = "roboshop-locking"
+
+  } 
+}
 provider "aws" {
   # Configuration options
   region = "us-east-1"
